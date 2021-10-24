29d96bbf9aa107cd02bb019351baa71dfccc0e10
Henrique-Arau <penielh43@gmail.com>
2021-10-23 22:48:14 -0300

    commit inicial

---
+++ src/main/java/com/Henrique/cocoBambu/CocoBambuApplication.java
@@ -0,0 +1,13 @@
+package com.Henrique.cocoBambu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocoBambuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocoBambuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Henrique/cocoBambu/CocoBambuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Henrique.cocoBambu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocoBambuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
