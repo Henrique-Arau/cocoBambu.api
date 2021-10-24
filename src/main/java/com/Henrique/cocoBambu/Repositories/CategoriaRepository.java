a763e171979e453c709646762aa8a0ec071c28d3
Henrique-Arau <penielh43@gmail.com>
2021-10-24 00:33:05 -0300

    repositories implementados

---
+++ src/main/java/com/Henrique/cocoBambu/Repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.Henrique.cocoBambu.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Henrique.cocoBambu.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
