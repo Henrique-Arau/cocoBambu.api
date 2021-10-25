a4b26d32e4e3308b5bea5b236cc9d9c1b6f21b67
Henrique-Arau <penielh43@gmail.com>
2021-10-25 00:18:30 -0300

    findById para categoria

---
+++ src/main/java/com/Henrique/cocoBambu/Service/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.Henrique.cocoBambu.Service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Henrique.cocoBambu.Repositories.CategoriaRepository;
+import com.Henrique.cocoBambu.domain.Categoria;
+
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+
+}
